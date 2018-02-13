e9ffd0807b0cb15d21accebdd1faf821640bbd2c
ki71 <ki71@cs32b017946.ad.susx.ac.uk>
2018-02-13 15:45:45 +0000

    Added inital classes

---
+++ PropertyTycoon/src/propertytycoon/PropertyTycoon.java
@@ -0,0 +1,17 @@
+package propertytycoon;
+
+/**
+ *
+ * @author 132206
+ */
+public class PropertyTycoon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //something added again
+    }
+    
+}
