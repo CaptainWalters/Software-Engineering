674df7ed6536db88d233df6a956c1592e9598c46
KJIreland <ki71@sussex.ac.uk>
2018-02-13 16:56:34 +0000

    Update Dice.java

--- Property_Tycoon/src/property_tycoon/Dice.java
+++ Property_Tycoon/src/property_tycoon/Dice.java
@@ -3,17 +3,13 @@ package property_tycoon;
 import java.util.Random;
 
 /**
- *
  * @author 132206
  */
 
 public class Dice {
  
-    
     public int rollDice(){
         Random rand = new Random();
-        
         return rand.nextInt(6)+1; 
     }
-    
 }
