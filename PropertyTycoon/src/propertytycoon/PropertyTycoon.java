db03cd3d4d9f4b2942aa89b8c17823fd4f6f4c35
ki71 <ki71@cs32b017946.ad.susx.ac.uk>
2018-02-13 15:48:02 +0000

    test 2

--- PropertyTycoon/src/propertytycoon/PropertyTycoon.java
+++ PropertyTycoon/src/propertytycoon/PropertyTycoon.java
@@ -11,7 +11,7 @@ public class PropertyTycoon {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //something added
+        //something added again
     }
     
 }
