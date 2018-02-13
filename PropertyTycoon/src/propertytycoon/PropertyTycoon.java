e310f814407362c2043ff779a3dc33b85e94a52a
ki71 <ki71@cs32b017946.ad.susx.ac.uk>
2018-02-13 15:45:45 +0000

    test

---
+++ PropertyTycoon/src/propertytycoon/PropertyTycoon.java
@@ -0,0 +1,16 @@
+package propertytycoon;
+
+/**
+ *
+ * @author 132206
+ */
+public class PropertyTycoon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
