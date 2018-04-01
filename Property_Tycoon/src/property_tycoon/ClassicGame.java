5d564cb9b7e379afc80ddc063f024608a2cfed85
Kieran <ki71@sussex.ac.uk>
2018-04-01 15:47:07 +0100

    First Revision of interface created. ClassicGame & AbridgedGame Classes created.
    Other necessery classes created.

---
+++ Property_Tycoon/src/property_tycoon/ClassicGame.java
@@ -0,0 +1,44 @@
+package property_tycoon;
+
+/**
+ *
+ * @author Kieran
+ */
+public class ClassicGame implements Property_Tycoon {
+
+    @Override
+    public Board initBoard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Player[] initPlayers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CPU[] initCPU() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Banker initBanker() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dice initDice() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public PotLuck[] initPotLuck() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public OpportunityKnocks[] initOpertunityKnocks() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
