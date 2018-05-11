66e75bcd4634d7a6e99b2399565485908a06cddc
KJIreland <ki71@sussex.ac.uk>
2018-05-11 13:00:12 +0100

    Javadocs for boardlocation

--- PropertyTycoon/src/BoardLocation.java
+++ PropertyTycoon/src/BoardLocation.java
@@ -3,10 +3,11 @@
  * @author Kieran(132206), Oliver(134730), Vlad (146674)
  *
  */
-public class BoardLocation {
 
-    // this class is used to populate each board location and can hold players on the space, what the space is
-    // and how much it costs etc.
+/**
+ * This class represents the individual location of the board. Such as Go, Properties, Pot luck etc.
+ */
+public class BoardLocation {
 
     private final int position;
     private final String name;
@@ -20,14 +21,15 @@ public class BoardLocation {
     private boolean mortgaged;
 
     /**
+     *This is the constructor for BoardLocation which loads the values for each individual property.
      *
-     * @param position
-     * @param name
-     * @param value
-     * @param rent
-     * @param colour
-     * @param action
-     * @param canBuy
+     * @param position The position on the board of the location.
+     * @param name The name of the location.
+     * @param value The cost to buy the location.
+     * @param rent An int array which holds the values of the rent, including with all improvements.
+     * @param colour The colour of the location space
+     * @param action The action performed when a player lands on the space.
+     * @param canBuy A boolean value stating whether or not a player can buy the location.
      */
     public BoardLocation(int position, String name, int value, int[] rent, String colour, String action, boolean canBuy){
         this.position = position;
@@ -43,7 +45,9 @@ public class BoardLocation {
         System.out.println(toString()); // DEBUG
     }
 
-    //@146674
+    /**
+     * This method overrides the default .toString() method and is used for printing about a description of the location.
+     */
     @Override
     public String toString() {
         return "Position: " + Integer.toString(position) + "\nName: " + name + "\nPrice to Buy: " + Integer.toString(locationValue)
@@ -52,49 +56,93 @@ public class BoardLocation {
                  + "\nColour: " + colour + "\nAction: " + action + "\nCan Player buy this: " + canBuy + "\nCurrent owner: " + owner + "\nProperty Mortgaged: " + mortgaged + "\n";
     }
 
+    /**
+     * This method returns the name of the location
+     *
+     * @return The name of the location.
+     */
     public String getName(){
         return name;
     }
 
-    //@146674
+    /**
+     * This method returns all the information refering to the location.
+     *
+     * @return All the information of the location.
+     */
     public String getInformation(){
         return this.toString();
     }
-     
-    //@146674
+
+    /**
+     * This method returns the colour of the location
+     *
+     * @return The colour of the location.
+     */
     public String getColour(){
         return this.colour;
     }
 
+    /**
+     * This method returns the price of the location
+     *
+     * @return The price of the location.
+     */
     public int getPrice(){
         return locationValue;
     }
-     
-    // getRentPrices() returns rent price according to houses/hotel propertyType
+
+    /**
+     * This method returns the rent price of the location, and takes into account whether the location has any houses or hotels.
+     *
+     * @return The price of rent of the location.
+     */
     public int getRentPrice(){
         return this.rentValue[ this.developments ]; // propertyType: [0] No house rent; [1] 1 house rent; ... ; [4] 4 houses rent; [5] Hotel rent
     }
 
-    // Get BoardLocation's owner (returns Player type object)
+    /**
+     * This method returns the owner of the location
+     *
+     * @return The owner of the location.
+     */
     public Player getOwner(){
         return this.owner;
     }
 
+    /**
+     * This method returns the boolean canBuy of the location.
+     *
+     * @return The boolean refering to whether someone can buy the location.
+     */
     public Boolean getCanBuy(){
         return this.canBuy;
     }
 
+    /**
+     * This method returns the number of developments on the location
+     *
+     * @return The number of developments "built" on the location.
+     */
     public int getDevelopments(){
         return this.developments;
     }
 
-    //@146674
-    // Location Action is now fed to Game::doAction() using value from this method
+    /**
+     * This method returns the action of the location
+     *
+     * @return The action of the location.
+     */
     public String getAction(){
         return this.action;
     }
 
     //@146674
+    /**
+     * This method returns the price of building a house or hotel on the location.
+     *
+     * @return The the price of building a house or hotel on the location.
+     */
     public int getHouseDevelopmentPrice(){
         int price = 0;
         switch( this.colour ){
@@ -132,23 +180,48 @@ public class BoardLocation {
         return price;
     }
 
+
+    /**
+     * This method returns a boolean referring to whether the location is currently mortgaged or not.
+     *
+     * @return The boolean referring to whether the location is currently mortgaged or not.
+     */
     public boolean getMortgaged(){
         return mortgaged;
     }
 
+    /**
+     * This method sets the mortgaged value to the value in the param.
+     *
+     * @param value true or false value that is being set.
+     */
     public void setMortgaged(boolean value){
         mortgaged = value;
     }
 
+    /**
+     * This method sets the owner of the location.
+     *
+     * @param player The player that owns the location.
+     */
     public void setOwner(Player player){
         owner = player;
     }
 
+    /**
+     * This method returns the isOwned boolean value.
+     *
+     * @return true if the location is owned by a player, otherwise null.
+     */
     public Boolean isOwned(){
         return (this.owner != null);
     } //Returns boolean defining if property is owned
-     
-    //@146674
+
+    /**
+     * This method places a house on the location.
+     *
+     * @param player The player that is buying a house.
+     */
     public Boolean buyHouse(Player player){
 
         // check if this location is maxed out with houses
@@ -162,10 +235,16 @@ public class BoardLocation {
         return true;
     }
 
+    /**
+     * This method sets the mortgaged value to true.
+     */
     public void mortgageLocation(){
            setMortgaged(true);
     }
 
+    /**
+     * This method sets the mortgaged value to false.
+     */
     public void unmortgageLocation(){
         setMortgaged(false);
     }
